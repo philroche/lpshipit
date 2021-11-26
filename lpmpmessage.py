9ea4cb2e6781dfc43287d00123297be1e88d90ca
Philip Roche <phil.roche@canonical.com>
2021-11-26 14:50:11 +0000

    Spike using Rich and Textual for MP review lists

--- lpmpmessage.py
+++ lpmpmessage.py
@@ -20,7 +20,19 @@ Activate your virtualenv and install the requirements::
 
 """
 import click
-import urwid
+from rich import box
+from rich.style import Style
+from rich.panel import Panel
+from rich.text import Text
+from rich.table import Table
+from textual.app import App
+from textual.events import Click, MouseDown
+from textual.reactive import Reactive
+from textual import events
+from textual.widget import Widget
+from textual.widgets import Header, Footer, Placeholder, ScrollView
+from textual.widgets import Button
+from typing import Optional
 
 from lpshipit import (
     build_commit_msg,
@@ -93,12 +105,67 @@ def summarize_all_mps(mps):
     return sorted_mps
 
 
+class Label(Widget):
+
+    label: Reactive[str] = Reactive('')
+
+    def __init__(self, label: str, name: Optional[str] = None):
+        super().__init__(name=name)
+        self.label = label
+
+    def render(self) -> Panel:
+        return Panel(self.label, box=box.SQUARE)
+
+    def on_click(self, event: Click) -> None:
+        event.prevent_default().stop()
+        self.label += 'clicked'
+
+
+class LPMPMessageApp(App):
+
+    async def on_load(self, event: events.Load) -> None:
+        """Bind keys with the app loads (but before entering application mode)"""
+        await self.bind("q", "quit", "Quit")
+
+    async def on_mount(self) -> None:
+        """Create and dock the widgets."""
+
+        # A scrollview to contain the markdown file
+        self.body = body = ScrollView(auto_width=True)
+
+        # Header / footer / dock
+        await self.view.dock(Footer(), edge="bottom")
+        await self.view.dock(body)
+
+        async def add_content():
+            table = Table(title="Choose Merge Proposal", box=box.ASCII, show_header=True, expand=True,
+                          row_styles=[Style(frame=True, bgcolor='red'), Style(frame=True, bgcolor='blue')])
+            table.add_column("[cyan]author",
+                             justify="left",
+                             no_wrap=True,)
+            table.add_column("[blue]repo",
+                             justify="left",
+                             no_wrap=False, )
+            table.add_column("[blue]button",
+                             justify="left",
+                             no_wrap=False, )
+            for i in range(19):
+
+                table.add_row("repo", "test label in table", Panel("Choose"))
+
+            await body.update(table)
+
+        await self.call_later(add_content)
+
+
 @click.command()
 @click.option('--mp-owner', help='LP username of the owner of the MP '
                                  '(Defaults to system configured user)',
               default=None)
 @click.option('--debug/--no-debug', default=False)
 def lpmpmessage(mp_owner, debug):
+    LPMPMessageApp.run(log="textual.log")
+
     lp = _get_launchpad_client()
     lp_user = lp.me
 
@@ -109,6 +176,8 @@ def lpmpmessage(mp_owner, debug):
         print('Debug: Launchad returned {} merge proposals'.format(len(mps)))
     mp_summaries = summarize_all_mps(mps)
     if mp_summaries:
+
+
         def urwid_exit_on_q(key):
             if key in ('q', 'Q'):
                 raise urwid.ExitMainLoop()
@@ -149,3 +218,8 @@ def lpmpmessage(mp_owner, debug):
 
 if __name__ == "__main__":
     lpmpmessage()
+
+
+
+
+
